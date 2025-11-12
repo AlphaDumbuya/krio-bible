80f45aed82be73433cbcceccf9abe7bed724e4ca
Alpha Dumbuya <alphadumbuya7@gmail.com>
2025-11-12 12:03:15 +0100

    Fix Metro bundler config - remove watchFolders with spaces in path

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,6 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
-
-// Add the parent directory to the watch folders so we can access the audio files
-config.watchFolders = [
-  path.resolve(__dirname, '../Krio audio bible new testament'),
-];
 
 // Add audio file extensions
 config.resolver.assetExts.push('mp3');
